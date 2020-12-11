93f80d66094bd154885e9bebbece21a65e6bdcc3
MozartMongi <saya.mozart@gmail.com>
2020-12-11 14:01:00 +0700

    deploy

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV == "development") {
+    require('dotenv').config()
+
+}
 const app = require('express') ();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
